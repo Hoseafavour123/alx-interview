bf2aafd638627d5c9264531b97d7b8e6f8988648
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-09-29 07:08:29 +0100

    pep8 validation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,24 +8,24 @@ def pascal_triangle(n):
         n (int): number of rows
     """
     pascal_tri = [[1], [1, 1]]
-    
+
     if n <= 0:
-        return [[]]
+        return []
     elif n == 1:
         return [pascal_tri[0]]
     elif n == 2:
         return pascal_tri
-    
+
     if n > 2:
         tmp = [1]
         for i in range(n - 2):
             tmp = [1]
-            
+
             for j in range(1, len(pascal_tri[-1])):
                 num = (pascal_tri[-1][j - 1] + pascal_tri[-1][j])
                 tmp.append(num)
-                    
+
             tmp.append(1)
             pascal_tri.append(tmp)
-            
-        return pascal_tri         
+
+        return pascal_tri
